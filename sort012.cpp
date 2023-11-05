3de72875744b5e4e6a18991a4bd2a4b3db40bef0
Saikat Shasmal <135151154+Dumisko@users.noreply.github.com>
2023-11-05 19:27:09 +0530

    Add files via upload

---
+++ sort012.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h> 
+void sort012(int *arr, int n)
+{
+   int count1=0;
+   int count0 =0;
+   int count2 =0;
+   for(int i=0;i<n;i++){
+      if(arr[i]==0){
+         count0++;
+      }
+      else if(arr[i]==1){
+         count1++;
+      }
+      else if(arr[i]==2){
+         count2++;
+      }
+   }
+   for(int i=0;i<n;i++){
+      if(count0){
+         arr[i]=0;
+         count0--;
+      }
+      else if(count1){
+         arr[i]=1;
+         count1--;
+      }
+      else if(count2){
+         arr[i]=2;
+         count2--;
+      }
+   }
+   
+}
